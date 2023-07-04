8948a3ea230ec68cea8bec0ea9062c8b880de96a
Eduard Baghdasaryan <eduardbaghdasaryan1995@gmail.com>
2023-07-04 15:10:26 +0400

    Added create swap, get swap and delete swap functionality

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -1,12 +1,74 @@
 import axios from "axios"
-import { prodHost } from "../env.dev.js";
+import { prodHost, apiKey } from "../env.dev.js";
 
 
 const getNetworks = async (req, res) => {
     try {
         const response = await axios.get(`${prodHost}/api/public/networks`, {
+            headers: {
+                'accept': `application/json'`,
+            }
+        });
+        res.json(response.data)
+    } catch (error) {
+        res.status(error.response.status).json({ error: error.response.data });
+    }
+}
+
+const getQuote = async (req, res) => {
+    const { source, destination, asset, refuel } = req.body
+
+    try {
+        const response = await axios.post(`${prodHost}/api/public/quote`, {
+            source,
+            destination,
+            asset,
+            refuel
+        }, {
+            headers: {
+                'accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        res.json(response.data)
+
+    } catch (error) {
+        res.status(error.response.status).json({ error: error.response.data });
+    }
+}
+
+const createSwap = async (req, res) => {
+    const { source, destination, amount, asset, sourceAddress,
+            destinationAddress, refuel, referenceId } = req.body
+    try {
+        const response = await axios.post(`${prodHost}/api/private/swaps`, {
+            source,
+            destination,
+            amount,
+            asset,
+            source_address: sourceAddress,
+            destination_address: destinationAddress,
+            refuel,
+            reference_id: referenceId
+        }, {
+            headers: {
+                'X-LS-APIKEY': apiKey,
+                'Content-Type': 'application/json'
+            }
+        });
+        res.json(response.data)
+    } catch (error) {
+        res.status(error.response.status).json({ error: error.response.data });
+    }
+}
+
+const getSwap = async (req, res) => {
+    try {
+        const {id} = req.params;
+        console.log(id);
+        const response = await axios.get(`${prodHost}/api/private/swaps/${id}`, {
           headers: {
-            'accept': `application/json'`,
+            'X-LS-APIKEY': apiKey
           }
         });
         res.json(response.data)
@@ -15,29 +77,24 @@ const getNetworks = async (req, res) => {
       }
 }
 
-const getQuote = async (req, res) => {
-    const {source, destination, asset, refuel} = req.body
-
+const deleteSwap = async (req, res) => {
     try {
-        const response = await axios.post(`${prodHost}/api/public/quote`, {
-          source,
-          destination,
-          asset,
-          refuel
-        }, {
+        const {id} = req.params;
+        const response = await axios.delete(`${prodHost}/api/private/swaps/${id}`, {
           headers: {
-            'accept': 'application/json',
-            'Content-Type': 'application/json'
+            'X-LS-APIKEY': apiKey
           }
         });
         res.json(response.data)
-
       } catch (error) {
-        res.status(error.response.status).json({ error:error.response.data });
+        res.status(error.response.status).json({ error: error.response.data });
       }
 }
 
 export {
     getNetworks,
-    getQuote
+    getQuote,
+    createSwap,
+    getSwap,
+    deleteSwap
 }
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,13 +1,13 @@
 import express from "express";
-import {getNetworks, getQuote} from "../controllers/controller.js";
+import {getNetworks, getQuote, createSwap, getSwap, deleteSwap} from "../controllers/controller.js";
 
 const router = express.Router();
 
 router.get("/networks", getNetworks);
 router.post("/quote", getQuote);
-router.post("/swaps",  postAbout);
-// router.get("/swaps/:id", controller.putAbout);
-// router.delete("swaps/:id", controller.deleteAbout);
+router.post("/swaps",  createSwap);
+router.get("/swaps/:id", getSwap);
+router.delete("/swaps/:id", deleteSwap);
 
 
 export default router;
