e04c10c292764b02e7db1011140318cf479573c2
David Hayriyan <david.hayriyan@goya.am>
2023-07-10 09:45:23 +0400

    Add socket io

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
+import { Server } from "socket.io";
+import { createServer } from 'http';
 
 import { allowedOrigin, port } from "./env.dev.js";
 import { sequelize } from "./src/model.js";
 import router from "./src/routes.js";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 
 app.use(express.json());
 app.use(helmet());
@@ -20,6 +24,14 @@ app.use(
 app.use(router);
 
 await sequelize.sync();
-app.listen(port, () => {
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is running on port ${port}.`);
 });
